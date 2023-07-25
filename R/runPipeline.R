ecfb612656c8880ddd7145102cdaaca9a241d918
unknown <jairo@jhmigueles.com>
2023-07-25 10:19:31 +0200

    replace stop by message and avoid running the comparison part of the pipeline if run for first time

--- R/runPipeline.R
+++ R/runPipeline.R
@@ -29,33 +29,36 @@ runPipeline = function(workdir = c(), datadir = c(), configfile = c(),
   prevDir = sort(files[-which(files == currentDir)], decreasing = TRUE)[1]
 
   # if not ever run before, output cannot be compared
-  if (is.na(prevDir)) stop("Next time you run this function, you will get a comparison with the output generated here.")
-
-  # Load GGIR results
-  currentOutput = prevOutput = list()
-  currentFN = dir(file.path(workdir, currentDir, "results"), pattern = "*.csv", full.names = TRUE)
-  prevFN = dir(file.path(workdir, prevDir, "results"), pattern = "*.csv", full.names = TRUE)
-
-  if (length(currentFN) != length(prevFN)) {
-    stop("Different number of output files were generated, make sure you used the same configfile.")
+  if (is.na(prevDir)) {
+    message("Next time you run this function, you will get a comparison with the output generated here.")
   } else {
-    for (i in 1:length(currentFN)) {
-      current = data.table::fread(currentFN[i])
-      prev = data.table::fread(prevFN[i])
 
-      # compare prev and current
-      outputFN = paste0(workdir, "/diff_in_", basename(basename(currentFN[i])))
-      outputFN = gsub(".csv", ".txt", outputFN)
-      if (i == 1) {}
-      issues = diffdf::diffdf(base = prev, compare = current,
-                              suppress_warnings = TRUE,
-                              file = outputFN)
-      areEqual = !diffdf::diffdf_has_issues(issues)
-      if (areEqual == TRUE) file.remove(outputFN)
+    # Load GGIR results
+    currentOutput = prevOutput = list()
+    currentFN = dir(file.path(workdir, currentDir, "results"), pattern = "*.csv", full.names = TRUE)
+    prevFN = dir(file.path(workdir, prevDir, "results"), pattern = "*.csv", full.names = TRUE)
 
-      cat("\nCheck files",
-          paste0("'", basename(currentFN)[i], "are identical:"),
-          areEqual)
+    if (length(currentFN) != length(prevFN)) {
+      stop("Different number of output files were generated, make sure you used the same configfile.")
+    } else {
+      for (i in 1:length(currentFN)) {
+        current = data.table::fread(currentFN[i])
+        prev = data.table::fread(prevFN[i])
+
+        # compare prev and current
+        outputFN = paste0(workdir, "/diff_in_", basename(basename(currentFN[i])))
+        outputFN = gsub(".csv", ".txt", outputFN)
+        if (i == 1) {}
+        issues = diffdf::diffdf(base = prev, compare = current,
+                                suppress_warnings = TRUE,
+                                file = outputFN)
+        areEqual = !diffdf::diffdf_has_issues(issues)
+        if (areEqual == TRUE) file.remove(outputFN)
+
+        cat("\nCheck files",
+            paste0("'", basename(currentFN)[i], "are identical:"),
+            areEqual)
+      }
     }
   }
 }
